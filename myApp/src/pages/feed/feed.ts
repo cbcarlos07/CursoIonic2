4ca25cc4a6f623c0976ef3894362f48f2fe6040d
Carlos Bruno <cbcarlos08@gmail.com>
2019-02-13 19:44:15 -0400

    Aula 35. Rodando app no simulador iphone

--- myApp/src/pages/feed/feed.ts
+++ myApp/src/pages/feed/feed.ts
@@ -2,13 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { MovieProvider } from '../../providers/movie/movie';
 
-/**
- * Generated class for the FeedPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-feed',
@@ -26,6 +19,7 @@ export class FeedPage {
       comments: 4,
       timeComments: '11h ago'
   }
+  listaFilmes = new Array<any>()
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
               private movieProvider: MovieProvider) {
@@ -37,8 +31,10 @@ export class FeedPage {
   ionViewDidLoad() {
      this.movieProvider.getLatestMovies()
                        .subscribe( data => {
-                          console.log(data);
-                          
+                         const response = (data as any)
+                         const obj_retorno = JSON.parse(response._body)
+                         this.listaFilmes = obj_retorno.results
+                         console.log(obj_retorno)
                        } )
   }
 
--- myApp/src/providers/movie/movie.ts
+++ myApp/src/providers/movie/movie.ts
@@ -2,12 +2,6 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 
-/*
-  Generated class for the MovieProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class MovieProvider {
   api_key = 'bcd9f1ea08ec22eca153e63973a0e9ef'
@@ -17,7 +11,9 @@ export class MovieProvider {
   }
 
   getLatestMovies(){
-    return this.http.get(`${this.baseApiPath}/movie/latest?api_key=${this.api_key}`)
+    
+    //return this.http.get(`${this.baseApiPath}/movie/latest?api_key=${this.api_key}`)
+    return this.http.get(`${this.baseApiPath}/movie/popular?api_key=${this.api_key}`)
   }
 
 }
