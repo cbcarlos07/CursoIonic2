70d0fa47367f82457487599a58abd5c891e4fff4
Carlos Bruno <cbcarlos08@gmail.com>
2019-02-12 20:33:51 -0400

    Aula 27. Alterando a pagina de feed para usar json

--- myApp/src/pages/feed/feed.ts
+++ myApp/src/pages/feed/feed.ts
@@ -14,7 +14,14 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'feed.html',
 })
 export class FeedPage {
-  public nome_usuario:string = "Carlos Bruno Brito";
+  feed = {
+      titulo:    'Carlos Bruno',
+      data:      'November 5, 1955',
+      descricao: 'Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine... out of a DeLorean?! Whoa. This is heavy.',
+      likes: 12,
+      comments: 4,
+      timeComments: '11h ago'
+  }
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
